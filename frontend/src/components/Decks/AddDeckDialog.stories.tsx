171c960726ec678524b93d8b29dc8bea05d8aeec
Jose <joseearronn@gmail.com>
2025-02-19 23:28:29 +0800

    test: add AddDeckDialog story

---
+++ frontend/src/components/Decks/AddDeckDialog.stories.tsx
@@ -0,0 +1,79 @@
+import { Meta, StoryFn } from "@storybook/react";
+import AddDeckDialog, { AddDeckDialogProps } from "./AddDeckDialog";
+import { useState } from "react";
+import { Dialog } from "@radix-ui/react-dialog";
+import { Toaster } from "react-hot-toast";
+
+const meta: Meta<typeof AddDeckDialog> = {
+  title: "Components/Decks/AddDeckDialog",
+  component: AddDeckDialog,
+  argTypes: {
+    onClose: { action: "closed" },
+    onSuccess: { action: "success" },
+  },
+};
+
+export default meta;
+
+const Template: StoryFn<AddDeckDialogProps> = (args) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const [loading, setLoading] = useState(args.loading);
+
+  return (
+    <>
+      <Toaster />
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <AddDeckDialog
+          {...args}
+          loading={loading}
+          onClose={() => setIsOpen(false)}
+          createDeck={async (...params) => {
+            if (args.loading) return;
+            try {
+              setLoading(true);
+              await args.createDeck(...params);
+            } catch {
+              throw new Error("Failed to create deck");
+            } finally {
+              setLoading(false);
+            }
+          }}
+        />
+      </Dialog>
+    </>
+  );
+};
+
+const delay = async (): Promise<void> => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  createDeck: async () => {
+    await delay();
+  },
+  loading: false,
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  loading: true,
+};
+
+export const WithError = Template.bind({});
+WithError.args = {
+  createDeck: async () => {
+    await delay();
+    throw new Error("Failed to create deck");
+  },
+  loading: false,
+};
+
+export const WithSuccess = Template.bind({});
+WithSuccess.args = {
+  createDeck: async () => {
+    await delay();
+  },
+  loading: false,
+};
